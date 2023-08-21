5718432445c45858a6a1393ca51aa87bfb293d98
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2023-08-22 03:07:00 +0900

    Support for changes to BetterMissCounter

--- BloomFontAssetMaker.cs
+++ BloomFontAssetMaker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
-namespace EnhancedMissCounter
+namespace BetterMissCounter
 {
     internal class BloomFontAssetMaker:PersistentSingleton<BloomFontAssetMaker>
     {
